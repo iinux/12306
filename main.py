83024dc414a3bdd8e3595310d75fdc7d9cb1f86f
iinux <iinux@qq.com>
2016-05-24 22:11:09 +0800

    more compatible with Linux, add config segment, add time print, move info to head in mail subject, catch more Exception,

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import smtplib
 import email
 import time
 import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 import os
 import platform
 
@@ -12,6 +14,13 @@ if platform.system() == "Windows":
 	isWindows = True
 else:
 	isWindows = False
+
+##############################
+# config
+##############################
+daemonMode = False
+internalSecond = 10
+##############################
 
 if isWindows:
 	import winsound
@@ -56,7 +65,7 @@ def trainTicket(from_station,to_station,date=[],seat=[],no_GD=False,email=False,
 	}
 	
 	for date_var in date:
-		print u"\n\n正在查询 "+date_var+u" 的车票...\n"
+		print u"\n\n [ " + now_time + " ] 正在查询 " + date_var + u" 的车票...\n"
 		# old url
 		url = 'https://kyfw.12306.cn/otn/leftTicket/queryT?leftTicketDTO.train_date='+date_var+'&leftTicketDTO.from_station='+stationCode[from_station]+'&leftTicketDTO.to_station='+stationCode[to_station]+'&purpose_codes=ADULT'
 		# 2016-05-23 update the url
@@ -99,18 +108,19 @@ def trainTicket(from_station,to_station,date=[],seat=[],no_GD=False,email=False,
 			for seat_var in seat:
 				if ii[seatCode[seat_var]]=='--':
 					continue
-				info=ii['station_train_code']+" [ "+ii['start_time']+" "+ii['arrive_time']+" "+ii['lishi']+" ] "+u"有 "+ii[seatCode[seat_var]]+u" 个 "+seat_var
-				#print info
+				info = ii['station_train_code'] + " [ " + ii['start_time'] + " " + ii['arrive_time'] + " " + ii['lishi'] + " ] " \
+					+ u"有 " + ii[seatCode[seat_var]] + u" 个 " + seat_var
+				print info
 				if ii[seatCode[seat_var]]==u'无':
 					continue
 
 				print info
 				if email:
-					sendmail(date_var+u" 从 "+from_station+u" 到 "+to_station+" "+info)
+					sendmail(info+" "+date_var+u" 从 "+from_station+u" 到 "+to_station)
 				if isWindows:
 					while True:
 						winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
-		print u'查询无车票，等待下一次查询'
+		print u'本次查询结束，等待下一次查询，' + str(internalSecond) + '秒之后'
 
 def sendmail(content):
 	myemail=smtplib.SMTP('smtp.139.com',25)
@@ -125,15 +135,71 @@ def sendmail(content):
 	myemail.sendmail('iinux@139.com',['iinux@139.com'],msg.as_string())
 	myemail.quit()
 
+
+'''将当前进程fork为一个守护进程
+   注意：如果你的守护进程是由inetd启动的，不要这样做！inetd完成了
+   所有需要做的事情，包括重定向标准文件描述符，需要做的事情只有chdir()和umask()了
+'''
+
+def daemonize (stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+     #重定向标准文件描述符（默认情况下定向到/dev/null）
+    try: 
+        pid = os.fork() 
+          #父进程(会话组头领进程)退出，这意味着一个非会话组头领进程永远不能重新获得控制终端。
+        if pid > 0:
+            sys.exit(0)   #父进程退出
+    except OSError, e: 
+        sys.stderr.write ("fork #1 failed: (%d) %s\n" % (e.errno, e.strerror) )
+        sys.exit(1)
+
+     #从母体环境脱离
+    os.chdir("/")  #chdir确认进程不保持任何目录于使用状态，否则不能umount一个文件系统。也可以改变到对于守护程序运行重要的文件所在目录
+    os.umask(0)    #调用umask(0)以便拥有对于写的任何东西的完全控制，因为有时不知道继承了什么样的umask。
+    os.setsid()    #setsid调用成功后，进程成为新的会话组长和新的进程组长，并与原来的登录会话和进程组脱离。
+
+     #执行第二次fork
+    try: 
+        pid = os.fork() 
+        if pid > 0:
+            sys.exit(0)   #第二个父进程退出
+    except OSError, e: 
+        sys.stderr.write ("fork #2 failed: (%d) %s\n" % (e.errno, e.strerror) )
+        sys.exit(1)
+
+     #进程已经是守护进程了，重定向标准文件描述符
+
+    for f in sys.stdout, sys.stderr: f.flush()
+    si = open(stdin, 'r')
+    so = open(stdout, 'a+')
+    se = open(stderr, 'a+', 0)
+    os.dup2(si.fileno(), sys.stdin.fileno())    #dup2函数原子化关闭和复制文件描述符
+    os.dup2(so.fileno(), sys.stdout.fileno())
+    os.dup2(se.fileno(), sys.stderr.fileno())
+
+if daemonMode:
+	daemonize('/dev/null','/tmp/daemon_stdout.log','/tmp/daemon_error.log')
+
+
 if isWindows:
 	winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
+sendmail(u"开始查询")
 
 while(True):
 	try:
-		trainTicket(u'石家庄',u'北京',['2016-02-13'],[u'二等座',u'硬座',u'硬卧'],email=False,fromTimeLimit=['12:00','23:59'],toTimeLimit=['00:00','22:00'])
-		time.sleep(10)
+		ltime = time.localtime()
+		now_time = time.strftime('%Y-%m-%d %X', ltime)
+
+		trainTicket(u'北京', u'邢台', ['2016-06-09'], [u'硬座'], email = True, fromTimeLimit = ['08:00', '11:59'], toTimeLimit = ['00:00', '18:00'])
+		time.sleep(internalSecond)
 	except KeyboardInterrupt:
-		print 'EXIT'
+		sys.stderr.write ( now_time + 'KeyboardInterrupt - EXIT\n\n' )
 		exit()
 	except urllib2.HTTPError:
+		sys.stderr.write ( now_time + ' urllib2.HTTPError' )
+		pass
+	except urllib2.URLError:
+		sys.stderr.write ( now_time + ' urllib2.URLError' )
+		pass
+	except Exception, e:
+		sys.stderr.write ( now_time + " " + str(Exception) + " " + str(e) )
 		pass
