5625a28297911559eaf5b8486343c71e7c4988e2
iinux <iinux@qq.com>
2018-01-21 15:44:17 +0800

    fmt

--- my_helper.py
+++ my_helper.py
@@ -12,6 +12,7 @@ if sys.version > '3':
 else:
     PY3 = False
 
+
 def dd(var):
     print(var)
     sys.exit(0)
@@ -62,7 +63,7 @@ def to_daemon(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
         pid = os.fork()
         # 父进程(会话组头领进程)退出，这意味着一个非会话组头领进程永远不能重新获得控制终端。
         if pid > 0:
-            sys.exit(0)   # 父进程退出
+            sys.exit(0)  # 父进程退出
     except (OSError) as e:
         error_output('fork #1 failed: (%d) %s\n' % (e.errno, e.strerror))
         sys.exit(1)
@@ -71,13 +72,13 @@ def to_daemon(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
     # chdir确认进程不保持任何目录于使用状态，否则不能umount一个文件系统。也可以改变到对于守护程序运行重要的文件所在目录
     os.chdir('/')
     os.umask(0)  # 调用umask(0)以便拥有对于写的任何东西的完全控制，因为有时不知道继承了什么样的umask。
-    os.setsid()    # setsid调用成功后，进程成为新的会话组长和新的进程组长，并与原来的登录会话和进程组脱离。
+    os.setsid()  # setsid调用成功后，进程成为新的会话组长和新的进程组长，并与原来的登录会话和进程组脱离。
 
     # 执行第二次fork
     try:
         pid = os.fork()
         if pid > 0:
-            sys.exit(0)   # 第二个父进程退出
+            sys.exit(0)  # 第二个父进程退出
     except (OSError) as e:
         error_output('fork #2 failed: (%d) %s\n' % (e.errno, e.strerror))
         sys.exit(1)
@@ -89,7 +90,7 @@ def to_daemon(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
     si = open(stdin, 'r')
     so = open(stdout, 'a+')
     se = open(stderr, 'a+', 0)
-    os.dup2(si.fileno(), sys.stdin.fileno())    # dup2函数原子化关闭和复制文件描述符
+    os.dup2(si.fileno(), sys.stdin.fileno())  # dup2函数原子化关闭和复制文件描述符
     os.dup2(so.fileno(), sys.stdout.fileno())
     os.dup2(se.fileno(), sys.stderr.fileno())
 
